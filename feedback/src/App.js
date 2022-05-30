76c5dff10f847b4f0563b536a97788f23301f084
Lorica7 <38511443+Lorica7@users.noreply.github.com>
2022-05-30 15:35:27 -0400

    delete function

--- feedback/src/App.js
+++ feedback/src/App.js
@@ -7,11 +7,12 @@ function App() {
 
   const [feedback, setFeedback] = useState({ FeedbackData });
   
-  const deleteFeedback = (id) => {
-    console.log(`app level ${id}`)
-    
-    
-}
+  const deleteFeedback = id => {
+  if (window.confirm ('Are you sure you want to delete?')) {
+    setFeedback (feedback.filter (item => item.id !== id));
+  }
+};
+
 
   return (
     <>
