992463b4f35e16e2c6be25acab333eff60dff72c
Lorica7 <38511443+Lorica7@users.noreply.github.com>
2022-06-03 09:27:22 -0400

    added rating component

--- feedback/src/components/FeedbackForm.jsx
+++ feedback/src/components/FeedbackForm.jsx
@@ -2,10 +2,25 @@ import React from 'react';
 import Card from './shared/Card.jsx';
 import {useState} from 'react';
 import Button from './shared/Button.jsx';
+import RatingSelect from './Rating-select.jsx';
 
 function FeedbackForm () {
   const [text, setText] = useState ('');
+  const [rating, setRating] = useState (10);
+  const [btnDisabled, setBtnDisabled] = useState (true);
+  const [message, setMessage] = useState ('');
+
   const handleTextChange = e => {
+    if (text === '') {
+      setBtnDisabled (true);
+      setMessage (null);
+    } else if (text !== '' && text.trim ().length <= 10) {
+      setMessage ('Text must be at least 10 characters');
+      setBtnDisabled (false);
+    } else {
+      setMessage (null);
+      setBtnDisabled (false);
+    }
     setText (e.target.value);
     console.log (text);
   };
@@ -13,6 +28,7 @@ function FeedbackForm () {
     <Card>
       <form>
         <h2 className="rate">How would you rate your service? </h2>
+        <RatingSelect select={rating => setRating (rating)} />
         <div className="input-group">
           <input
             onChange={handleTextChange}
@@ -20,8 +36,11 @@ function FeedbackForm () {
             placeholder="Write a Review"
             value={text}
           />
-          <Button className="send" type="submit"> Send</Button>
+          <Button className="send" type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
